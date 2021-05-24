bd71886896d51f3188fc8dd90d3bb490f3652a62
Bryn Knowles <bryn.knowles@gmail.com>
2021-05-24 17:34:23 -0400

    changed location of navbar

--- src/App.js
+++ src/App.js
@@ -2,31 +2,34 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Projects from './Projects.js'; 
 import Articles from './Articles.js'; 
 import About from './About.js';
-import './App.css';
+// import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <header>
-      <h1>Bryn Knowles</h1>
-      <Router>
-        <div className="App">
-          <Route exact path="/" component={Projects} />
-          <Route path="/articles" component={Articles} />
-          <Route path="/about" component={About} />
+    <div className="App">
+      <header>
+        <h1>Bryn Knowles</h1>
+        <Router>
+          <NavBar />
 
-          <div className="navigation">
-            {/* <img src={logo} className="logo" alt="Logo Image" /> */}
-            <div className="navigation-sub">
-              <Link to="/" className="item">Projects</Link>
-              <Link to="/articles" className="item">Articles</Link>
-              <Link to="/about" className="item">About</Link>
-            </div>
-          </div>
+            <Route exact path="/" component={Projects} />
+            <Route path="/articles" component={Articles} />
+            <Route path="/about" component={About} />
 
-        </div>
-      </Router>
-    </header>
+            {/* <div className="navigation"> */}
+              {/* <img src={logo} className="logo" alt="Logo Image" /> */}
+              {/* <div className="navigation-sub">
+                <Link to="/" className="item">Projects</Link>
+                <Link to="/articles" className="item">Articles</Link>
+                <Link to="/about" className="item">About</Link>
+              </div> */}
+            {/* </div> */}
 
+
+        </Router>
+      </header>
+    </div>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
 
 function NavBar() {
     return(
-        <div>
-            <h1>Logo</h1>
-            <h1>Name</h1>
-            <p>About</p>
-            <p>Portfolio</p>
-            <p>Contact</p>
+        // <div>
+        //     <h1>Logo</h1>
+        //     <h1>Name</h1>
+        //     <p>About</p>
+        //     <p>Portfolio</p>
+        //     <p>Contact</p>
+        // </div>
+        <div className="navigation">
+            <div className="navigation-sub">
+            <Link to="/" className="item">Projects</Link>
+            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/about" className="item">About</Link>
+            </div>
         </div>
-
     )
 }
+
+export default NavBar;
