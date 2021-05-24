b6ac1e7fe45a043bd57d84cbc85cc6dda06ebbe1
Bryn Knowles <bryn.knowles@gmail.com>
2021-05-24 17:41:29 -0400

    deleted old code

--- src/App.js
+++ src/App.js
@@ -12,21 +12,9 @@ function App() {
         <h1>Bryn Knowles</h1>
         <Router>
           <NavBar />
-
             <Route exact path="/" component={Projects} />
             <Route path="/articles" component={Articles} />
             <Route path="/about" component={About} />
-
-            {/* <div className="navigation"> */}
-              {/* <img src={logo} className="logo" alt="Logo Image" /> */}
-              {/* <div className="navigation-sub">
-                <Link to="/" className="item">Projects</Link>
-                <Link to="/articles" className="item">Articles</Link>
-                <Link to="/about" className="item">About</Link>
-              </div> */}
-            {/* </div> */}
-
-
         </Router>
       </header>
     </div>
