15edffddaa73b30ced6f509152da2910b458293e
Bryn Knowles <bryn.knowles@gmail.com>
2021-06-01 15:57:53 -0400

    added images and fleshed out project objects

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 import ProjectCard from "../components/ProjectCard";
-import LPImg from '../images/LitterPickerImage.png'
+import LtrPkrImg from '../images/LitterPicker.png';
+import PortfolioImg from '../images/PortfolioFirst.png';
+// import MonorailTCGImg from '../images/MonorailTCG.png'
+import BrelbysImg from '../images/Brelbys.png';
+import RICShopImg from '../images/RubyIceCreamShop.png';
+
 
 function ProjectContainer() {
 
@@ -9,42 +14,37 @@ function ProjectContainer() {
 
     const projectsArray = [
         {
-            // link: "/litterpicker",
-            imageURL: LPImg,
+            imageURL: LtrPkrImg,
             title: "LitterPicker",
-            summary: "A single page web app where a user can create litter cleanup events",
+            summary: "A single page web app where a user can create litter cleanup events. Built with Ruby on Rails for the backend, React.js and CSS Flexbox for the frontend.",
             code: "code",
             demo: "demo"
         },
         {
-            // link: "/my-portfolio",
-            imageUrl: "Project imageURL",
+            imageUrl: PortfolioImg,
             title: "My Portfolio",
-            summary: "",
+            summary: "This is my personal portfolio. Built with React.js and Styled Components.",
             code: "code",
             demo: "demo"
         },
         {
-            // link: "/monorail",
+            imageUrl: BrelbysImg,
+            title: "Brelby's",
+            summary: "A single page web app where a user can shop for wine and cupcakes, with recommendations for pairings of the two. Built with Ruby on Rails and styled using Vanilla CSS.",
+            code: "code",
+            demo: "demo"
+        },
+        {
             imageUrl: "Project imageUrl",
             title: "MonorailTCG",
-            summary: "",
+            summary: "A single page web app where a user can create Simpsons trading cards for their collection. Built with Vanilla JS.",
             code: "code",
             demo: "demo"
         },
         {
-            // link: "/brelby",
-            imageUrl: "Project imageUrl",
-            title: "Brelby",
-            summary: "",
-            code: "code",
-            demo: "demo"
-        },
-        {
-            // link: "/ruby-icecream-shop",
-            imageUrl: "Project imageUrl",
+            imageUrl: RICShopImg,
             title: "Ruby Ice Cream Shop",
-            summary: "",
+            summary: "A single page CLI app where a user can order ice cream. Built with Ruby.",
             code: "code",
             demo: "demo"
         },
