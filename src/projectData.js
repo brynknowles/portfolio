eaa116e35a05aae659dd5bf137b3b074591fa885
Bryn Knowles <bryn.knowles@gmail.com>
2021-06-09 11:43:58 -0400

    some changes to data file

--- src/projectData.js
+++ src/projectData.js
@@ -16,7 +16,7 @@ const projects = [
     {
         id: 2,
         name: "My Portfolio",
-        about: "My personal project portfolio.",
+        summary: "My personal project portfolio.",
         primaryTechnology: "React",
         link: null,
         code: null,
@@ -26,7 +26,7 @@ const projects = [
     {
         id: 3,
         name: "Brelby's",
-        about: "Shop for wine and cupcakes, with recommendations for pairings.",
+        summary: "Shop for wine and cupcakes, with recommendations for pairings.",
         primaryTechnology: "Ruby on Rails",
         link: null,
         code: null,
@@ -36,7 +36,7 @@ const projects = [
     {
         id: 4,
         name: "MonorailTCG",
-        about: "Create Simpsons trading cards to add to your collection.",
+        summary: "Create Simpsons trading cards to add to your collection.",
         primaryTechnology: "Javascript",
         link: null,
         code: null,
@@ -46,7 +46,7 @@ const projects = [
     {
         id: 5,
         name: "Ruby Ice Cream Shop",
-        about: "Create and order an ice cream cone of your choosing.",
+        summary: "Create and order an ice cream cone of your choosing.",
         primaryTechnology: "Ruby",
         link: null,
         code: null,
